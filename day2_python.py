43e7f10b47472901103352eecd92e52ed3559388
aafifarajput <aafifarajput@gmail.com>
2026-01-08 21:58:18 +0500

    Add files via upload

---
+++ day2_python.py
@@ -0,0 +1,47 @@
+# type of variable
+fruits=20
+weight=35.7
+
+print(type(fruits)) #integer
+print(type(weight)) #float
+
+
+
+# operators
+print(5+6) #addition
+print(5-6) #subtraction
+print(5*6) #multipliation
+print(5/6) #true division quotient of a and b 
+print(5//6) #floor divsion quotient of a and b removing fractional parts
+print(5%6)  #modulus return remainder
+print(5**6) #exponent
+print(-5)   #negation
+
+
+"""order of operators :
+P parenthesis
+E exponents
+M multiplication/divsion
+D
+A addition/subtraction
+S
+ """
+
+
+#builtin functions
+
+print(abs(-5))
+print(abs(5))
+
+print(min(1,2,3))
+print(max(1,2,3))
+
+print(float(10)) #converting int to float
+print(int(23.4567)) #converting float to integer
+
+# calculate area
+pi= 3.14159
+diameter =36
+radius=diameter/2
+area=pi*radius*2
+print(area)
